399a5ebc65b7297d6ee06631e1a294c54f6f2e6e
petual1 <132956221+petual1@users.noreply.github.com>
2023-05-09 18:35:14 -0300

    loading animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,10 @@
-const horas = document.getElementById("horas");
-const minutos = document.getElementById("minutos");
-const segundos = document.getElementById("segundos");
+const horas = document.querySelector(".horas");
+const minutos = document.querySelector(".minutos");
+const segundos = document.querySelector(".segundos");
 const paisElement = document.getElementById("pais");
 const estadoElement = document.getElementById("estado");
 
-const relogio = setInterval(function time() {
-  let dateToday = new Date();
-  let hr = dateToday.getHours();
-  let min = dateToday.getMinutes();
-  let sg = dateToday.getSeconds();
-
-  if (hr < 10) hr = '0' + hr;
-  if (min < 10) min = '0' + min;
-  if (sg < 10) sg = '0' + sg;
-
-  horas.textContent = hr;
-  minutos.textContent = min;
-  segundos.textContent = sg;
-
+function obterLocalizacao() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(
       position => {
@@ -48,4 +35,22 @@ const relogio = setInterval(function time() {
   } else {
     console.error('Navegador não suporta geolocalização.');
   }
-}, 1000);
+}
+
+function atualizarRelogio() {
+  const dateToday = new Date();
+  let hr = dateToday.getHours();
+  let min = dateToday.getMinutes();
+  let sg = dateToday.getSeconds();
+
+  if (hr < 10) hr = '0' + hr;
+  if (min < 10) min = '0' + min;
+  if (sg < 10) sg = '0' + sg;
+
+  horas.textContent = hr;
+  minutos.textContent = min;
+  segundos.textContent = sg;
+}
+
+obterLocalizacao();
+setInterval(atualizarRelogio, 1000);
