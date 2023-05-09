d2aae3ff5932bccafbf8713a17d9e9f57cb5acf1
petual1 <132956221+petual1@users.noreply.github.com>
2023-05-09 17:26:01 -0300

    api bug fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,43 +5,47 @@ const paisElement = document.getElementById("pais");
 const estadoElement = document.getElementById("estado");
 
 const relogio = setInterval(function time() {
-    let dateToday = new Date();
-    let hr = dateToday.getHours();
-    let min = dateToday.getMinutes();
-    let sg = dateToday.getSeconds();
+  let dateToday = new Date();
+  let hr = dateToday.getHours();
+  let min = dateToday.getMinutes();
+  let sg = dateToday.getSeconds();
 
-    if (hr < 10) hr = '0' + hr;
-    if (min < 10) min = '0' + min;
-    if (sg < 10) sg = '0' + sg;
+  if (hr < 10) hr = '0' + hr;
+  if (min < 10) min = '0' + min;
+  if (sg < 10) sg = '0' + sg;
 
-    horas.textContent = hr;
-    minutos.textContent = min;
-    segundos.textContent = sg;
+  horas.textContent = hr;
+  minutos.textContent = min;
+  segundos.textContent = sg;
 
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                const { latitude, longitude } = position.coords;
-                const apiKey = '76b56adcec854a77a2863374a27e79d7';
-                const apiUrl = `https://api.opencagedata.com/geocode/v1/json?key=${apiKey}&q=${latitude},${longitude}`;
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
 
-                fetch(apiUrl)
-                    .then(response => response.json())
-                    .then(data => {
-                        const country = data.results[0].components.country;
-                        const state = data.results[0].components.state;
-                        paisElement.textContent = country;
-                        estadoElement.textContent = state;
-                    })
-                    .catch(error => {
-                        console.error('Erro ao obter informações de geolocalização:', error);
-                    });
-            },
-            error => {
-                console.error('Erro ao obter a localização:', error);
-            }
-        );
-    } else {
-        console.error('Navegador não suporta geolocalização.');
-    }
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            const address = response.address;
+
+            const state = address.state;
+            const country = address.country;
+
+            paisElement.textContent = country;
+            estadoElement.textContent = state;
+          }
+        };
+
+        xhr.send();
+      },
+      error => {
+        console.error('Erro ao obter a localização:', error);
+      }
+    );
+  } else {
+    console.error('Navegador não suporta geolocalização.');
+  }
 }, 1000);
